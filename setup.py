0f304203efab523f01889040e94dee197fd44183
Sebastian Noack <sebastian.noack@gmail.com>
2018-08-27 20:38:24 +0200

    Don't test on Python 3.2 and 3.3 anymore, as it fails due to incompatible tooling

--- setup.py
+++ setup.py
@@ -14,14 +14,7 @@ setup(
         'License :: Public Domain',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
