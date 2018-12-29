1fdd9e0ea0a562dd88680d59004e2016b3dac6a8
Sebastian Noack <sebastian.noack@gmail.com>
2018-12-29 13:36:32 -0500

    Add README contents as long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as file:
+    long_description = file.read()
 
 setup(
     name='goto-statement',
     version='1.1',
     url='https://github.com/snoack/python-goto/',
     description='A function decorator, that rewrites the bytecode, to enable goto in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     py_modules=['goto'],
     author='Sebastian Noack',
     author_email='sebastian.noack@gmail.com',
